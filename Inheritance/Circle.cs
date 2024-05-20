0180366fab9ac9232a859d9306f7080895c0f976
Dophian <169108628+Dophian@users.noreply.github.com>
2024-05-20 16:31:12 +0900

    게임 엔진 프레임 워크 프로젝트 추가
    - 다형성의 예제를 살펴보기 위한 프로젝트
    - Component 타입을 다형성을 이용해서 구성함

--- Inheritance/Circle.cs
+++ Inheritance/Circle.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Inheritance
+public class Circle : Point
 {
-    internal class Circle
+    private float radius;
+    private const float PI = 3.1415926535f;
+
+    public Circle(float x, float y, float radius)
     {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public override void ShowData()
+    {
+        base.ShowData();
+
+        Console.WriteLine($"반지름: {radius}");
+        Console.WriteLine($"원의 넓이: {PI * radius * radius}");
     }
 }
