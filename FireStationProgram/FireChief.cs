f56beb0638133e53b8573bc866985fc899db9c6d
Dophian <169108628+Dophian@users.noreply.github.com>
2024-05-21 15:02:27 +0900

    객체지향 프로그래밍 상속 및 보호까지 진행함.
    - 수습 소방관 클래스를 추가하고, 객체 지향의 다형성 원리를 코드에 적용

---
+++ FireStationProgram/FireChief.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 소방서장 클래스.
+// 소방관이면서 다른 소방관에게 불을 끄도록 지시할 수 있는 권한이 추가됨.
+public class FireChief : Firefighter
+{
+    // 필드 - 1순위 소방관. 
+    public Firefighter NumberOne {  get; set; }
+
+    // 불끄기 함수.
+    public override void ExtinguishFire()
+    {
+        //Console.WriteLine($"{Name} 소방관이 불을 끄고 있습니다.");
+        // 직접 불을 끄는 대신, 1순위 소방관에게 불끄도록 지시한다.
+        TellFirefighterToExtinguishFire(NumberOne);
+    }
+
+    // 공개 - 다른 소방관에게 불을끄라고 지시하는 메소드(인터페이스).
+    // 관계 - 의존(Dependency) - 메소드 호출할 때 Firefighter를 전달받고 기능을 사용함.
+    public void TellFirefighterToExtinguishFire(Firefighter colleague)
+    {
+        colleague.ExtinguishFire();
+    }
+}
---
+++ FireStationProgram/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    // 프로그램 진입점(Entry Point).
+    static void Main(string[] args)
+    {
+        // 소방관 객체.
+        Firefighter johnny = new Firefighter { Name = "Johnny" };
+        Firefighter james = new Firefighter { Name = "James" };
+
+        // 소방차 객체.
+        Firetruck truckOne = new Firetruck();
+        truckOne.Driver = johnny;
+
+        // 운전.
+        johnny.Drive(truckOne, new Point(3, 4));
+
+        // 불끄기.
+        johnny.ExtinguishFire();
+        james.ExtinguishFire();
+
+        // ... //
+        truckOne.Driver = james;
+
+        // 운전 시도.
+        johnny.Drive(truckOne, new Point(10, 2));
+
+        // 소방서장 객체 생성.
+        //FireChief fireChief = new FireChief { Name = "Harry",NumberOne = johnny };
+        Firefighter fireChief = new FireChief { Name = "Harry",NumberOne = johnny };
+        truckOne.Driver = fireChief;
+        fireChief.Drive(truckOne, new Point(20, 30));
+
+        // 소방서장이 다른 소방관에게 불끄라고 지시함.
+        //fireChief.TellFirefighterToExtinguishFire(johnny);
+        fireChief.ExtinguishFire();
+
+        // Harry 소방서장을 참조해 사용함. Firefighter 타입으로 사용.
+        // 업캐스팅(Upcasting) - 다형성 활용.
+        //Firefighter stillHarry = fireChief;
+        //Firefighter joe = new Firefighter { Name = "Joe" };
+        // 업캐스팅을 한 후에는 부모 클래스가 가진 기능만 사용이 가능하다.
+        //stillHarry.TellFirefighterToExtinguishFire(joe);
+
+        // 수습 소방관 객체 생성.
+        Firefighter bill = new TraineeFirefighter { Name = "Bill" };
+        bill.ExtinguishFire();
+
+        Console.ReadKey();
+    }
+}
